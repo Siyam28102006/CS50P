42ca4d6412a33a5d2a27b427d51a0f65112c1144
Siyam UwU <shahriarsiyam50@gmail.com>
2026-03-26 12:13:21 +0600

    cleaned up repo - keeping only projects folder

--- basics/main.py
+++
@@ -1,3 +0,0 @@
-#this is my first python program
-print("I love pizza")
-print("It's really good")
--- basics/time counter.py
+++
@@ -1,9 +0,0 @@
-import time
-
-j=1
-n=int(input("Enter n: "))
-for i in range(0,n):
-    print(i)
-
-    time.sleep(j)
-    j+=1
