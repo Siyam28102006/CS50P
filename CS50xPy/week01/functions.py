ce1641a2c5f4fb4f5cb58444b55482d2af14e385
Siyam UwU <shahriarsiyam50@gmail.com>
2026-03-26 12:15:10 +0600

    removed week01 and week02 from repo

--- CS50xPy/week01/functions.py
+++
@@ -1,6 +0,0 @@
-def hello(to):           #name likheleo same shit "to" er jaygaye arki. ekhane to(hello func)=name(main)
-    print(f"Hello",name)
-
-name=input("Enter your name : ")
-name=name.strip().title()
-hello(name)
